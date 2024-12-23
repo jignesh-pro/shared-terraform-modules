23b696a335aa23ed78d177187fb2c4c038da4f45
Jigneshkumar Patel <jignesh.cisco@hotmail.com>
2024-12-23 10:09:31 +0530

    init acm

--- main.tf
+++ main.tf
@@ -1,229 +1,74 @@
-//get region
-data "aws_region" "current" {}
-
 locals {
-  common_name = "${var.environment}-${var.project}-${var.application_name}"
+  create_certificate          = var.create_certificate && var.putin_khuylo
+  create_route53_records_only = var.create_route53_records_only && var.putin_khuylo
+
+  # Get distinct list of domains and SANs
+  distinct_domain_names = coalescelist(var.distinct_domain_names, distinct(
+    [for s in concat([var.domain_name], var.subject_alternative_names) : replace(s, "*.", "")]
+  ))
+
+  # Get the list of distinct domain_validation_options, with wildcard
+  # domain names replaced by the domain name
+  validation_domains = local.create_certificate || local.create_route53_records_only ? distinct(
+    [for k, v in try(aws_acm_certificate.this[0].domain_validation_options, var.acm_certificate_domain_validation_options) : merge(
+      tomap(v), { domain_name = replace(v.domain_name, "*.", "") }
+    )]
+  ) : []
 }
 
-//Create Cloudwatch Group for ECS service Logs
-resource "aws_cloudwatch_log_group" "ecs_service_log_group" {
-  name              = "/ecs/${local.common_name}-SVCECSLogGroup"
-  retention_in_days = var.retention_in_days
-  tags              = merge(var.tags, { Name = "${local.common_name}-SVCECSLogGroup" })
-}
+resource "aws_acm_certificate" "this" {
+  count = local.create_certificate ? 1 : 0
 
-resource "random_integer" "four_digit_number" {
-  min = 1000
-  max = 9999
-}
+  domain_name               = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+  validation_method         = var.validation_method
+  key_algorithm             = var.key_algorithm
 
-//Create Security Group for ECS Service
-resource "aws_security_group" "ecs_service_sg" {
-  name        = "${local.common_name}-${random_integer.four_digit_number.result}-SVCECSSG"
-  description = "Security Group for ECS Service"
-  vpc_id      = var.vpc_id
-  ingress {
-    from_port   = var.application_port
-    to_port     = var.application_port
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = merge(var.tags, { Name = "${local.common_name}-SVCECSSG" })
-}
-
-resource "aws_ecs_task_definition" "api_task_definition" {
-  family                   = "${local.common_name}-ECSTaskDefinition"
-  execution_role_arn       = var.execution_role_arn
-  task_role_arn            = var.task_role_arn
-  network_mode             = "awsvpc"
-  cpu                      = var.ecs_cpu
-  memory                   = var.ecs_memory
-  requires_compatibilities = ["FARGATE"]
-  container_definitions = jsonencode([
-    {
-      name                   = var.application_name
-      image                  = var.application_ecr_image
-      cpu                    = tonumber(var.ecs_cpu)
-      memory                 = tonumber(var.ecs_memory)
-      enable_execute_command = true
-      essential              = true
-      portMappings = [
-        {
-          containerPort = tonumber(var.application_port)
-          hostPort      = tonumber(var.application_port)
-          protocol      = var.application_protocol
-          name          = "http"
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          "awslogs-group"         = aws_cloudwatch_log_group.ecs_service_log_group.name
-          "awslogs-region"        = data.aws_region.current.name
-          "awslogs-stream-prefix" = "ecs"
-          "mode"                  = "non-blocking"
-          "max-buffer-size"       = "25m"
-        }
-      }
-    }
-  ])
-  tags = merge(var.tags, { Name = "${local.common_name}-API-ECSTaskDefinition" })
-}
-
-//create ecs service
-resource "aws_ecs_service" "ecs_service" {
-  name            = "${local.common_name}-svc"
-  cluster         = var.ecs_cluster_arn
-  task_definition = aws_ecs_task_definition.api_task_definition.arn
-  desired_count   = var.container_desired_count
-  network_configuration {
-    subnets          = var.private_subnets
-    security_groups  = [aws_security_group.ecs_service_sg.id]
-    assign_public_ip = false
+  options {
+    certificate_transparency_logging_preference = var.certificate_transparency_logging_preference ? "ENABLED" : "DISABLED"
   }
 
-  capacity_provider_strategy {
-    capacity_provider = var.environment == "prod" ? "FARGATE" : "FARGATE_SPOT"
-    weight            = 1
-  }
+  dynamic "validation_option" {
+    for_each = var.validation_option
 
-  dynamic "load_balancer" {
-    for_each = var.application_type == "external" ? [1] : []
     content {
-      target_group_arn = aws_lb_target_group.ecs_service_target_group[0].arn
-      container_name   = var.application_name
-      container_port   = var.application_port
+      domain_name       = try(validation_option.value["domain_name"], validation_option.key)
+      validation_domain = validation_option.value["validation_domain"]
     }
   }
 
-  deployment_maximum_percent         = 200
-  deployment_minimum_healthy_percent = 100
-  enable_ecs_managed_tags            = true
-  enable_execute_command             = true
-  propagate_tags                     = "SERVICE"
-  scheduling_strategy                = "REPLICA"
+  tags = var.tags
 
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-
-  service_registries {
-    registry_arn = aws_service_discovery_service.service_discovery_service.arn
-  }
-
-  # service_connect_configuration {
-  #   enabled   = true
-  #   namespace = var.private_dns_namespace_id
-
-  # service {
-  #   port_name      = "http"
-  #   discovery_name = var.application_name
-  #   client_alias {
-  #     port = var.application_port
-  #   }
-  # }
-  # }
-
-  tags = merge(var.tags, { Name = "${local.common_name}-svc" })
   lifecycle {
-    ignore_changes = [
-      desired_count,
-      task_definition,
-      # load_balancer,
-      # service_registries,
-      # service_connect_configuration,
-    ]
+    create_before_destroy = true
   }
 }
 
-// Create Service Discovery Service for API Service
-resource "aws_service_discovery_service" "service_discovery_service" {
-  name = var.application_name
-  dns_config {
-    namespace_id = var.private_dns_namespace_id
-    dns_records {
-      ttl  = 10
-      type = "A"
-    }
-    routing_policy = "MULTIVALUE"
-  }
-  health_check_custom_config {
-    failure_threshold = 1
-  }
+resource "aws_route53_record" "validation" {
+  count = (local.create_certificate || local.create_route53_records_only) && var.validation_method == "DNS" && var.create_route53_records && (var.validate_certificate || local.create_route53_records_only) ? length(local.distinct_domain_names) : 0
 
-  tags = merge(var.tags, { "Name" = "${local.common_name}-Service" }, )
+  zone_id = lookup(var.zones, element(local.validation_domains, count.index)["domain_name"], var.zone_id)
+  name    = element(local.validation_domains, count.index)["resource_record_name"]
+  type    = element(local.validation_domains, count.index)["resource_record_type"]
+  ttl     = var.dns_ttl
+
+  records = [
+    element(local.validation_domains, count.index)["resource_record_value"]
+  ]
+
+  allow_overwrite = var.validation_allow_overwrite_records
+
+  depends_on = [aws_acm_certificate.this]
 }
 
-//Create a target group for the ECS service
-resource "aws_lb_target_group" "ecs_service_target_group" {
-  count       = var.application_type == "external" ? 1 : 0
-  name        = "${local.common_name}-SVCECS-TG"
-  port        = var.application_port
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
+resource "aws_acm_certificate_validation" "this" {
+  count = local.create_certificate && var.validation_method != null && var.validate_certificate && var.wait_for_validation ? 1 : 0
 
-  health_check {
-    path                = var.health_check.path
-    port                = var.health_check.port
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-  tags = merge(var.tags, { Name = "${local.common_name}-SVCECSTargetGroup" })
-}
+  certificate_arn = aws_acm_certificate.this[0].arn
 
-//Create ECS Service Autoscaling Policy
-resource "aws_appautoscaling_target" "ecs_service_target" {
-  max_capacity       = var.container_max_count
-  min_capacity       = var.container_desired_count
-  resource_id        = "service/${var.ecs_cluster_name}/${aws_ecs_service.ecs_service.name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-}
+  validation_record_fqdns = flatten([aws_route53_record.validation[*].fqdn, var.validation_record_fqdns])
 
-//Create ECS Service Autoscaling Policy for CPU
-resource "aws_appautoscaling_policy" "ecs_cpu_service_policy" {
-  name               = "${var.application_name}-CPUECSServiceScalingPolicy"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_service_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_service_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_service_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
-    target_value       = 60.0
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 300
-  }
-}
-
-//Create ECS Service Autoscaling Policy for Memory
-resource "aws_appautoscaling_policy" "ecs_memory_service_policy" {
-  name               = "${var.application_name}-MemoryECSServiceScalingPolicy"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_service_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_service_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_service_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-    target_value       = 60.0
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 300
+  timeouts {
+    create = var.validation_timeout
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,147 +1,130 @@
-variable "environment" {
-  description = "The environment in which the resources are being created"
+variable "create_certificate" {
+  description = "Whether to create ACM certificate"
+  type        = bool
+  default     = true
+}
+
+variable "create_route53_records_only" {
+  description = "Whether to create only Route53 records (e.g. using separate AWS provider)"
+  type        = bool
+  default     = false
+}
+
+variable "validate_certificate" {
+  description = "Whether to validate certificate by creating Route53 record"
+  type        = bool
+  default     = true
+}
+
+variable "validation_allow_overwrite_records" {
+  description = "Whether to allow overwrite of Route53 records"
+  type        = bool
+  default     = true
+}
+
+variable "wait_for_validation" {
+  description = "Whether to wait for the validation to complete"
+  type        = bool
+  default     = true
+}
+
+variable "validation_timeout" {
+  description = "Define maximum timeout to wait for the validation to complete"
   type        = string
-  default     = "dev"
-
-  validation {
-    condition     = contains(["dev", "qa", "uat", "prod"], var.environment)
-    error_message = "The environment must be one of 'dev', 'qa', 'uat', or 'prod'."
-  }
+  default     = null
 }
 
-variable "project" {
-  description = "The name of the project"
+variable "certificate_transparency_logging_preference" {
+  description = "Specifies whether certificate details should be added to a certificate transparency log"
+  type        = bool
+  default     = true
+}
+
+variable "domain_name" {
+  description = "A domain name for which the certificate should be issued"
   type        = string
-  default     = "test"
+  default     = ""
 }
 
-variable "application_name" {
-  description = "The name of the application"
-  type        = string
-  default     = "api"
-}
-
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
-
-variable "retention_in_days" {
-  description = "The number of days to retain log events"
-  type        = number
-  default     = 7
-}
-
-variable "application_port" {
-  description = "The port on which the application listens"
-  type        = number
-  default     = 8080
-}
-
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "vpc_id" {
-  description = "The ID of the VPC"
-  type        = string
-}
-
-variable "private_subnets" {
-  description = "A list of private subnets inside the VPC"
+variable "subject_alternative_names" {
+  description = "A list of domains that should be SANs in the issued certificate"
   type        = list(string)
   default     = []
 }
 
-variable "execution_role_arn" {
-  description = "The ARN of the IAM role to use for the ECS task execution"
+variable "validation_method" {
+  description = "Which method to use for validation. DNS or EMAIL are valid. This parameter must not be set for certificates that were imported into ACM and then into Terraform."
   type        = string
-
-}
-
-variable "task_role_arn" {
-  description = "The ARN of the IAM role to use for the ECS task"
-  type        = string
-}
-
-variable "ecs_cpu" {
-  description = "The amount of CPU to reserve for the ECS task"
-  type        = string
-  default     = "256"
-
-}
-
-variable "ecs_memory" {
-  description = "The amount of memory to reserve for the ECS task"
-  type        = string
-  default     = "512"
-
-}
-
-variable "application_ecr_image" {
-  description = "The ECR image to use for the ECS task"
-  type        = string
-
-}
-
-variable "ecs_cluster_arn" {
-  description = "The name of the ECS cluster ARN"
-  type        = string
-
-}
-
-variable "ecs_cluster_name" {
-  description = "The name of the ECS cluster"
-  type        = string
-
-}
-
-variable "container_desired_count" {
-  description = "The desired count of the container"
-  type        = number
-  default     = 1
-
-}
-
-variable "container_max_count" {
-  description = "The maximum count of the container"
-  type        = number
-  default     = 2
-}
-
-variable "health_check" {
-  description = "The health check configuration for the target group"
-  type = object({
-    path = string
-    port = number
-  })
-  default = {
-    path = "/"
-    port = 80
-  }
-}
-
-variable "private_dns_namespace_id" {
-  description = "The ID of the private DNS namespace"
-  type        = string
-
-}
-
-variable "application_type" {
-  description = "The type of the application Allowed values are external or internal"
-  type        = string
-  default     = "internal"
+  default     = null
 
   validation {
-    condition     = contains(["external", "internal"], var.application_type)
-    error_message = "The environment must be one of 'external' or 'internal'."
+    condition     = var.validation_method == null || contains(["DNS", "EMAIL"], coalesce(var.validation_method, 0))
+    error_message = "This variable is optional. Valid values are DNS, EMAIL, or null."
   }
 }
 
-variable "application_protocol" {
-  description = "value of the protocol"
+variable "validation_option" {
+  description = "The domain name that you want ACM to use to send you validation emails. This domain name is the suffix of the email addresses that you want ACM to use."
+  type        = any
+  default     = {}
+}
+
+variable "create_route53_records" {
+  description = "When validation is set to DNS, define whether to create the DNS records internally via Route53 or externally using any DNS provider"
+  type        = bool
+  default     = true
+}
+
+variable "validation_record_fqdns" {
+  description = "When validation is set to DNS and the DNS validation records are set externally, provide the fqdns for the validation"
+  type        = list(string)
+  default     = []
+}
+
+variable "zone_id" {
+  description = "The ID of the hosted zone to contain this record. Required when validating via Route53"
   type        = string
+  default     = ""
+}
+
+variable "zones" {
+  description = "Map containing the Route53 Zone IDs for additional domains."
+  type        = map(string)
+  default     = {}
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
+
+variable "dns_ttl" {
+  description = "The TTL of DNS recursive resolvers to cache information about this record."
+  type        = number
+  default     = 60
+}
+
+variable "acm_certificate_domain_validation_options" {
+  description = "A list of domain_validation_options created by the ACM certificate to create required Route53 records from it (used when create_route53_records_only is set to true)"
+  type        = any
+  default     = {}
+}
+
+variable "distinct_domain_names" {
+  description = "List of distinct domains and SANs (used when create_route53_records_only is set to true)"
+  type        = list(string)
+  default     = []
+}
+
+variable "key_algorithm" {
+  description = "Specifies the algorithm of the public and private key pair that your Amazon issued certificate uses to encrypt data"
+  type        = string
+  default     = null
+}
+
+variable "putin_khuylo" {
+  description = "Do you agree that Putin doesn't respect Ukrainian sovereignty and territorial integrity? More info: https://en.wikipedia.org/wiki/Putin_khuylo!"
+  type        = bool
+  default     = true
 }
